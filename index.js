5d8e9d5e40c40c4778ce84e1e1a62ccb58bda9da
MMariamidze <104030318+MMariamidze@users.noreply.github.com>
2022-06-21 01:57:14 +0400

    up

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const link = 'https://randomuser.me/api?results=50';
 
 GetData()
 
 async function GetData() {
     const response = await fetch('https://randomuser.me/api?results=50');
-    const data = await response.json();
-    GetData(data.results);
-}
+    const {results} = await response.json();
 
-function GetData (info){
-    info.forEach((info) =>{
-        const {first,email, gender,age, country,} = info;
+    results.forEach(info =>{
+
         const user = document.createElement('div')
-        user.classList.add('user')
+        user.classList.add('userInfo')
+
         user.innerHTML = `
-        <img src ="${info.picture.large}" alt ="${info.name.first}">
-        <div class="user-info">
+            <div class="card">
+            <div class="card-body">
+            <div class ="cardimg">
+            <img src ="${info.picture.large}" alt ="${info.name.first}">
+            </div>
             <h2>${info.name.first} ${info.name.last}</h2>
-            <p>${info.email}</p>
-            <p>${"gender" + info.gender} ${"age" + info.age}</p>
-            <p>${"country" + info.country}
-        
+            <p>${ "connect:" + " " +info.email}</p>
+            <p>${"gender:" + " " + info.gender}</p>
+            <p>${"age:" + " " + info.dob.age}</p>
+            <p>${"country:" + " " + info.location.country}</p>
+            <p>${"city:" + " " + info.location.city}</p> 
+            </div>    
+            </div>
         `;
         main.appendChild(user)
-
     })
 }
+
+form.addEventListener ('submit' , srch => {
+    srch.preventDefault()
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== ' '){
+        GetData(link + searchTerm )
+        search.value = ' '
+    }
+    else{
+        window.location.reload()
+    }
+})
